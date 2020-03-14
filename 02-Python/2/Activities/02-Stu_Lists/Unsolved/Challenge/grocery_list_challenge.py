a54e80651d75616e8db35b5f5184c131b3ac4201
ben u <benu@bens-MacBook-Pro.local>
2020-03-14 18:23:11 -0500

    checking in 2.2

---
+++ 02-Python/2/Activities/02-Stu_Lists/Unsolved/Challenge/grocery_list_challenge.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Student Do: Grocery List.
+
+This script showcases basic operations of Python Lists to help Sally
+organize her grocery shopping list.
+"""
+
+# @TODO: Create a list of groceries
+grocery_list = ['water','butter','eggs','apples','cinnamon ','sugar','milk']
+
+
+
+
+# @TODO: Find the first two items on the list
+
+print [:2]
+
+
+# @TODO: Find the last five items on the list
+
+print len[:-5]
+
+
+# @TODO: Find every other item on the list, starting from the second item
+
+print [0:2]
+
+
+# @TODO: Add an element to the end of the list
+
+print append
+
+
+
+# @TODO: Changes a specified element within the list at the given index
+
+
+
+
+
+# @TODO: Calculate how many items you have in the list
+
+
+
+
+# ----------------------Go to the grocery store---------------------------")
+
+# @TODO: Find the index of the particular element name
+
+
+
+
+
+# @TODO: Remove an element from the list based on the given element name
+
+
+
+
+
+# @TODO: Remove an element from the list based on the given index
+
+
+
+
+
+
+# @TODO: Remove the last element of the list
+
+
+
+
+
+print("You continue on your journey purchasing groceries...")
